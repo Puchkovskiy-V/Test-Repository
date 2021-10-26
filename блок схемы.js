8b76db0c84f934c56f9207cf0223678063054dda
Vlad Puchkovskiy <91284356+Puchkovskiy-V@users.noreply.github.com>
2021-10-26 21:17:48 +0300

    Create блок схемы.js

---
+++ блок схемы.js
@@ -0,0 +1,31 @@
+
+/*primer 1
+let x = 9;
+let y = 3;
+x = Math.sqrt(x);
+y**=2;
+x+=1;
+y+=x;
+console.log(y) */
+
+
+ //primer 2
+/*
+ let x = -8
+if (x>0) {y=2*x
+}
+else {y=-2*x}   
+console.log(y)*/
+
+//primer 3
+/*
+let a = 0;
+let b = 1;
+if (a<5){
+ a +=b ;
+a++;
+}
+else{a}
+  
+console.log(a)
+*/
